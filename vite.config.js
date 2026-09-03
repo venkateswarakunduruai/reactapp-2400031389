bf0e3388fe6ab52c477de6f69ee3e35521e5105f
venkateswarakundurai <venkateswarakunduru.ai@gmail.com>
2026-09-03 11:28:03 +0530

    navbar updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactapp/',
+  base: '/reactapp-2400031389/',
 })
